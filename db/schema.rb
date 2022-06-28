049c57ba4913fe4849eaca59ddbbe9d4f815f58d
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-28 15:41:37 +0200

    Create Food model and add a validation model test

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_131914) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_133815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "Name"
+    t.string "Measurement_unit"
+    t.integer "Price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "Name"
