75527d92a64f36034b2a872fc69d8ec6fe730a04
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-06-28 15:55:52 +0200

    Add models tests to verify associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :Name
+
+  has_many :recipe
+  has_many :food
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ describe User, type: :model do
   describe 'validations of User Model' do
     it { should validate_presence_of :Name }
   end
+
+  describe 'relationships' do
+    it { should have_many :food }
+    it { should have_many :recipe }
+  end
 end
